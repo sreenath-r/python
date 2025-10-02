770cdca98772cfbed842815d3fedae8a50600070
sreenath kumar <iamsreenathkumar@gmail.com>
2025-10-02 07:08:57 +0530

    variablespractice

---
+++ day1.py
@@ -0,0 +1,17 @@
+#Variables
+
+#what will be the output of following
+Age1=5                      #1
+print(Age1)
+
+5age=55                     #2
+print(5age)
+#variable names cannot start with a number
+
+Age_1=100                   #3
+print(Age_1)
+
+age@1=100                   #4
+print(age@1)
+#variable names cannot contain special characters like @, #, $, %, etc.
+
